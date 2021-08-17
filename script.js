f309d13c9ced5280de9d7c853af9a602f3b1ebb9
meltrust <original.mtapia@outlook.com>
2021-08-17 17:39:54 -0500

    Add gitignore, functions for local storage

--- script.js
+++ script.js
@@ -1,42 +1,33 @@
-let collection = [];
-
-class Book {
-    constructor(title, author) {
-        this.title = title,
-        this.author = author,
-        collection.push({ 'title': this.title, 'author': this.author });
-    }
+function allBooks() {
+  let books;
+  if (localStorage.getItem('books') === null) {
+    books = [];
+  } else {
+    books = JSON.parse(localStorage.getItem('books'));
+  }
+  return books;
 }
 
-let book1 = Book('title1', 'mr writer');
-let book2 = Book('title2', 'mr writer');
-
-function displayBook(title, author, id) {
-    let books = document.getElementById('books');
-    let article = document.createElement('article');
-    let h2 = document.createElement('h2');
-    let p = document.createElement('p');
-    let button = document.createElement('button');
-
-    books.append(article);
-    article.append(h2, p, button);
-
-    button.setAttribute('id', id)
-    button.setAttribute('onclick', 'removeBook(this.id)')
-
-    h2.textContent = title;
-    p.textContent = author;
-    button.textContent = 'remove';
-
-    function removeBook(id) {
-        // collection = collection.filter(book => book.title !== title);
-        collection = collection.splice(id, 1);
-        article.remove();
-    }
+function addBooks(newBook) {
+  const getAllBooks = allBooks();
+  getAllBooks.push(newBook);
+  localStorage.setItem('books', JSON.stringify(getAllBooks));
 }
 
-console.log(collection[1]);
+function deleteBooks(title, author) {
+  const books = allBooks();
 
-for (let i = 0; i <= collection.length; i++) {
-    displayBook(collection[i].title, collection[i].author, i);
+  books.forEach((book, index) => {
+    if (book.title === title && book.author === author) {
+      books.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+// Display on UI
+
+function addBookToList(book) {
+
 }
