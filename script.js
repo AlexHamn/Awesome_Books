455c638ac4f72f7e8997851854fbe30c65a6237c
AlexHamn <agalexandro16@gmail.com>
2021-08-18 13:51:47 -0500

    add local storage functionality

--- script.js
+++ script.js
@@ -1,33 +1,69 @@
-function allBooks() {
-  let books;
-  if (localStorage.getItem('books') === null) {
-    books = [];
-  } else {
-    books = JSON.parse(localStorage.getItem('books'));
+const addButton = document.getElementById('addButton');
+const booksList = document.getElementById('booksList');
+const addAuthor = document.getElementById('addAuthor');
+const addTitle = document.getElementById('addTitle');
+
+let books;
+
+if (localStorage.getItem('books') === null) {
+  books = [];
+} else {
+  books = JSON.parse(localStorage.getItem('books'));
+}
+
+function displayBooks() {
+  books.forEach((book) => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+    <h2>${book.title}</h2>  
+    <p>${book.author}</p>  
+    <button class="remove">remove</button>
+    <hr>`;
+    booksList.append(li);
+  });
+}
+
+function addBook(e) {
+  e.preventDefault();
+  const title = addTitle.value;
+  const author = addAuthor.value;
+  const book = {
+    title,
+    author,
+  };
+  book.id = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+  books.push(book);
+  localStorage.setItem('books', JSON.stringify(books));
+  addTitle.value = '';
+  addAuthor.value = '';
+  const li = document.createElement('li');
+  li.classList.add('book');
+  li.dataset.id = book.id;
+  li.innerHTML = `
+    <h2>${book.title}</h2>  
+    <p>${book.author}</p>  
+    <button class="remove">remove</button>
+    <hr>`;
+  booksList.append(li);
+}
+
+function removeBook(elem) {
+  if (elem.classList.contains('remove')) {
+    elem.parentElement.remove();
   }
-  return books;
 }
 
-function addBooks(newBook) {
-  const getAllBooks = allBooks();
-  getAllBooks.push(newBook);
-  localStorage.setItem('books', JSON.stringify(getAllBooks));
-}
+document.addEventListener('DOMContentLoaded', displayBooks);
 
-function deleteBooks(title, author) {
-  const books = allBooks();
+addButton.addEventListener('click', addBook);
 
-  books.forEach((book, index) => {
-    if (book.title === title && book.author === author) {
-      books.splice(index, 1);
+booksList.addEventListener('click', (e) => {
+  removeBook(e.target);
+  const newBooks = JSON.parse(localStorage.getItem('books'));
+  newBooks.forEach((book, index) => {
+    if (book.author === e.target.previousElementSibling.textContent) {
+      newBooks.splice(index, 1);
     }
   });
-
-  localStorage.setItem('books', JSON.stringify(books));
-}
-
-// Display on UI
-
-function addBookToList(book) {
-
-}
+  localStorage.setItem('books', JSON.stringify(newBooks));
+});
